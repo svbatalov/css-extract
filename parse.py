15068e3f37f6477879fe3b5ed4784d491cbf6aa0
Batalov Sergey <firstfire@list.ru>
2013-10-24 01:55:50 +0600

    Cleanup.
    Removed old definitions.
    Removed .text field from Rule class -- replaced by more "exact"
    .start / .end fields (no whitespace losses).

--- parse.py
+++ parse.py
@@ -5,10 +5,6 @@ from pyparsing import Word, Literal, Forward, ZeroOrMore, OneOrMore, Group,\
     SkipTo, alphas, cStyleComment, originalTextFor, MatchFirst, CharsNotIn, Suppress, delimitedList
 import pyparsing as pp
 
-def rule_action(s, l, t):
-    #print(s, l, t)
-    t.start = l
-
 # http://pyparsing.wikispaces.com/share/view/54406740
 # http://sourceforge.net/mailarchive/forum.php?thread_name=4E8B3555.1090404%40cs.wisc.edu&forum_name=pyparsing-users
 class CSSNode(object):
@@ -21,35 +17,27 @@ class CSSNode(object):
         return self.__class__.__name__ + ':' + str(self.__dict__)
     __repr__ = __str__
 
-class Selector(CSSNode):
-    def assign_fields(self):
-        self.text = self.token[0]
-        del self.token
-
 class Rule(CSSNode):
     def assign_fields(self):
         print("rule: tok 1", self.token[0], "tok 2:", self.token[1])
         sel = self.token[0]
         self.sel  = list(map(lambda x: x.strip(), sel.split(',')))
-        self.text = self.token[0]
         self.end = pp.getTokensEndLoc()
         for e in self.token[1]:
-            if hasattr(e, 'text'):
-                self.text += e.text
+            if hasattr(e, 'sel'):
                 self.sel  += e.sel      # collect selectors from inner blocks
-            else:
-                self.text += e
         del self.token
 
 def make_action(cls):
     def action(s, l, t):
         return cls(t, l)
     return action
+
 rule    = Forward()
 body    = OneOrMore(CharsNotIn('{};') + ';')
 sel     = CharsNotIn('{};')
 
-rule    << sel('sel') + Group( originalTextFor('{') + ZeroOrMore(rule|body('body')) + '}')
+rule    << sel + Group( '{' + ZeroOrMore( rule | body ) + '}' )
 
 rule.setParseAction( make_action(Rule) )
 
@@ -91,7 +79,6 @@ css = open('./bootstrap.css').read()
 rules = stylesheet.parseString(css)
 pprint(rules.asList())
 for r in rules:
-    print(r.text)
     print(r.sel, r.start, r.end)
     print(css[r.start : r.end])
 
